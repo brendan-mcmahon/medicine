bbf0d48915effb558cc5cc41334154219fa7d182
brendan.mcmahon.bloomerang <90283736+brendanjeffreymcmahon@users.noreply.github.com>
2025-04-03 12:54:36 -0400

    add logging for time check logic

--- CheckMedication/index.js
+++ CheckMedication/index.js
@@ -11,37 +11,41 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient({
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
 function isBeforeNineAM(timestamp) {
-  const date = new Date(timestamp);
-  const easternDate = new Date(date.toLocaleString('en-US', { timeZone: 'America/New_York' }));
-  return easternDate.getHours() < 9;
+	console.log('Timestamp:', timestamp);
+	const date = new Date(timestamp);
+	console.log('Date:', date);
+	const easternDate = new Date(date.toLocaleString('en-US', { timeZone: 'America/New_York' }));
+	console.log('Eastern Date:', easternDate);
+	console.log('Eastern Date Hours:', easternDate.getHours());
+	return easternDate.getHours() < 9;
 }
 
 exports.handler = async () => {
 	console.log('Checking medication...');
-  try {
-    const params = {
-      TableName: 'Medicine',
-    };
+	try {
+		const params = {
+			TableName: 'Medicine',
+		};
 
-    const data = await dynamoDb.scan(params).promise();
-	console.log('Data:', data);
+		const data = await dynamoDb.scan(params).promise();
+		console.log('Data:', data);
 
-    for (const item of data.Items) {
-      if (isBeforeNineAM(item.lastMedicatedTime)) {
-        await bot.sendMessage(item.chatId, 'Reminder: You have not taken your medication today.');
-		console.log('Sent message to:', item.chatId);
-      }
-    }
+		for (const item of data.Items) {
+			if (isBeforeNineAM(item.lastMedicatedTime)) {
+				await bot.sendMessage(item.chatId, 'Reminder: You have not taken your medication today.');
+				console.log('Sent message to:', item.chatId);
+			}
+		}
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Medication check completed' })
-    };
-  } catch (error) {
-    console.error('Error checking medication:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Internal server error' })
-    };
-  }
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ message: 'Medication check completed' })
+		};
+	} catch (error) {
+		console.error('Error checking medication:', error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ message: 'Internal server error' })
+		};
+	}
 };
